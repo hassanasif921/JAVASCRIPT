95a04bbcbc4ab10f2efa7044e42dd52440d60034
hassanasif921 <hasssanasif302672@gmail.com>
2020-06-04 22:21:30 +0500

    Variable For String

---
+++ Chapter 2/app.js
@@ -0,0 +1,36 @@
+// Task 1
+var username;
+// task 2
+ var myName = "Hassan Asif";
+ // Task 3
+
+ var message ="Hello World";
+ //alert(message);
+
+ //task 4
+
+ username ="Jhone Doe";
+ var age ="15 Years Old";
+ var course ="Certified Mobile App Development";
+ //alert(username);
+ //alert(age);
+ //alert(course);
+ 
+//Task 5
+
+//task 6
+
+var email ="example@example.com";
+//alert ("My Email Address Is "+ email);
+
+//task 7
+
+var book ="A smarter way to learn JavaScript";
+//alert("I Am Trying To learn From The Book "+book);
+
+//task 8
+//document.write("Yah! I can write HTML Content Through JavaScript");
+
+//task 9
+//alert("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬");
+
