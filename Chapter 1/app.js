e618d92a3acb3bf90f75aa3d0e981869ef8e8fc5
hassanasif921 <hasssanasif302672@gmail.com>
2020-06-04 18:32:59 +0500

    Alert

---
+++ Chapter 1/app.js
@@ -0,0 +1,17 @@
+//Task 1
+//alert("Hello");
+
+// Task 2
+//alert("Error ! Please Enter A Valid Password");
+
+// Task 3
+//alert("WelCome To JS Land.. \r\n Happy Coading.");
+
+// Task 4
+//alert("WelCome To JS Land..");
+//alert("Happy Coading.");
+
+// Task 5
+//alert("Hello .. I Can Run JS through my developer Console");
+
+
