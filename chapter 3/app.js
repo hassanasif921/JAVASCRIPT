44f22ae7269640ca36c08b9eade9648c5de27829
hassanasif921 <hasssanasif302672@gmail.com>
2020-06-06 01:13:57 +0500

    Variable For Numbers

---
+++ chapter 3/app.js
@@ -0,0 +1,19 @@
+// Task 1
+
+var age = 15;
+//alert("I Am "+age+" Years Old");
+
+//Task 2
+
+var visitors =14;
+//alert("You Have Visited This Site "+visitors+" Time")
+
+//Task 3
+//var birthyear= 2002;
+//document.write("My Birth Year Is "+birthyear+"<br> Data Type of my declared Variable is number");
+
+//Task 4
+var name ="Hassan";
+var product ="Shirt";
+var quantity ="15";
+document.write(name +" Ordered " +product+" " +quantity + " on XYZ cloting Store");
